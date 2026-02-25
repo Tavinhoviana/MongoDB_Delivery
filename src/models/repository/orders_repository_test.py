58a0c7ac85ca54485792b4261478a909852993a4
Otávio Viana Marques dos Santos <otavio_vms@hotmail.com>
2026-02-25 15:41:07 -0300

    test: implementing unit tests in orders repository

---
+++ src/models/repository/orders_repository_test.py
@@ -0,0 +1,43 @@
+from .orders_repository import OrdersRepository
+
+class CollectionMock:
+    def __init__(self) -> None:
+        self.insert_one_attributes = {}
+        self.find_attributes = {}
+
+
+    def insert_one(self, input_data: any):
+        self.insert_one_attributes["dict"] = input_data
+
+    def find(self, *args):
+        self.find_attributes["args"] = args
+
+class DBCollectionMock:
+    def __init__(self, collection) -> None:
+        self.get_collection_attributes = {}
+        self.collection = collection
+
+    def get_collection(self, collection_name) -> None:
+        self.get_collection_attributes["name"] = collection_name
+        return self.collection
+
+def test_insert_document():
+    collection = CollectionMock()
+    db_connection = DBCollectionMock(collection)
+    repo = OrdersRepository(db_connection)
+
+    doc = { "alguma": "coisa"}
+    repo.insert_document(doc)
+
+    assert collection.insert_one_attributes["dict"] == doc
+
+def test_select_many_with_properties():
+    collection = CollectionMock()
+    db_connection = DBCollectionMock(collection)
+    repo = OrdersRepository(db_connection)
+
+    doc = {" testando: " "find"}
+    repo.select_many_with_properties(doc)
+
+    assert collection.find_attributes["args"][0] == doc
+    assert collection.find_attributes["args"][0] == doc
