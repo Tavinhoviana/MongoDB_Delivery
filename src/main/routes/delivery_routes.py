2ee707d66ec877dc9ef23abbdca7e64cf57a8407
Otávio Viana Marques dos Santos <otavio_vms@hotmail.com>
2026-02-26 09:31:33 -0300

    feat: implementing registry order composer

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -6,4 +6,3 @@ delivery_routes_bp = Blueprint("delivery_routes", __name__)
 @delivery_routes_bp.route("/delivery/order", methods=["POST"])
 def registry_order():
     http_request = HttpRequest(body=request.json)
-    
