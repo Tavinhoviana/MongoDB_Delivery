74e708187fb39e24801a22dd914616b58e3012ee
Otávio Viana Marques dos Santos <otavio_vms@hotmail.com>
2026-03-04 13:15:18 -0300

    feat: implementing validators

--- src/validators/registry_order_validator.py
+++ src/validators/registry_order_validator.py
@@ -4,42 +4,23 @@ def registry_order_validator(body: any):
     body_validator = Validator({
         "data": {
             "type": "dict",
-            "required": True,
             "schema": {
-                "name": {
-                    "type": "string",
-                    "required": True
-                },
-                "address": {
-                    "type": "string",
-                    "required": True
-                },
-                "cupom": {
-                    "type": "boolean",
-                    "required": True
-                },
+                "name": { "type": "string", "required": True },
+                "address": { "trpe": "string", "required": True},
+                "cupom": { "type": "boolean", "required": True},
                 "items": {
-                    "type": "list",
-                    "required": True,
+                    "type": "list"},
                     "schema": {
                         "type": "dict",
                         "schema": {
-                            "item": {
-                                "type": "string",
-                                "required": True
-                            },
-                            "quantidade": {
-                                "type": "integer",
-                                "required": True
-                            }
+                            "items": { "type": "string", "required": True},
+                            "quantidade": { "type": "integer", "required": True}
                         }
                     }
-                }
             }
         }
     })
 
     response = body_validator.validate(body)
-
     if response is False:
         raise Exception(body_validator.errors)
